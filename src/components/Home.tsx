b3285d9577728a74a7e772a69a0e12c2653baaa7
Sungho <hurgj123kr@gmail.com>
2024-10-04 20:23:04 +0900

    today 변수 정리하고 week변수로 변환 하는일이 같음

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,6 @@ import WeatherChart from "./WeatherChart";
 import { WeatherContainer } from "../styles/CommonStyles";
 import { main } from "../api";
 import { getWeatherCode, getWeatherBackgroundImage } from "../weatherCode";
-import weatherUv from "../weatherUv";
 
 // css
 const WeatherLi = styled.ul`
@@ -62,18 +61,6 @@ const Home: React.FC = () => {
   const [showWeatherInfo, setShowWeatherInfo] = useState<boolean>(true); // 기본적으로 보여주는 날씨데이터
   const [showChart, setShowChart] = useState<boolean>(false); // 그래프 형태의 날씨데이터.
   const [selectedDate, setSelectedDate] = useState<number>(0);
-
-  // 특정 날짜인지 확인하는 함수
-  const isToday = (timestamp: string): boolean => {
-    const date = new Date(timestamp);
-    const today = new Date();
-
-    return (
-      date.getUTCFullYear() === today.getUTCFullYear() &&
-      date.getUTCMonth() === today.getUTCMonth() &&
-      date.getUTCDate() === today.getUTCDate()
-    );
-  };
 
   // 시간을 "오전/오후 몇 시" 형식으로 변환하는 함수
   const formatTimeTo12Hour = (timestamp: string): string => {
@@ -147,25 +134,22 @@ const Home: React.FC = () => {
     setShowChart(false);
   };
 
-  // 시간대별 데이터 필터링 (오늘 날짜만)
-  const todayTimes = weatherData.hourly.time.filter((time: string) =>
-    isToday(time)
+  // 해당 날짜 시간대별 습도, 날씨상황, 풍속
+  const selectedDayHumidity = weatherData.hourly.relative_humidity_2m.slice(
+    selectedDate * 24,
+    (selectedDate + 1) * 24
   );
 
-  const todayWindspeed = weatherData.hourly.wind_speed_10m.slice(
-    0,
-    todayTimes.length
+  const selectedDayWeatherCodes = weatherData.hourly.weathercode.slice(
+    selectedDate * 24,
+    (selectedDate + 1) * 24
   );
 
-  const todayHumidity = weatherData.hourly.relative_humidity_2m.slice(
-    0,
-    todayTimes.length
+  const selectedDayWindspeed = weatherData.hourly.wind_speed_10m.slice(
+    selectedDate * 24,
+    (selectedDate + 1) * 24
   );
 
-  const todayWeatherCodes = weatherData.hourly.weathercode.slice(
-    0,
-    todayTimes.length
-  );
   const todayDay = selectedDayWeather.date.getUTCDate();
   const todayMonth = selectedDayWeather.date.getMonth() + 1;
 
@@ -191,7 +175,7 @@ const Home: React.FC = () => {
     const formattedHours = hours % 12 || 12; // 12시간 형식으로 변환
     const min = date.getMinutes();
     if (date.getMinutes() < 10) {
-      const min = `"0"+${date.getMinutes()}`;
+      const min = `0${date.getMinutes()}`;
       return `${formattedHours}:${min}`;
     }
     return `${formattedHours}:${min}`;
@@ -206,6 +190,22 @@ const Home: React.FC = () => {
     };
   });
 
+  // 해당 날짜 해당 시간대 온도
+  const getCurrentTemperature = () => {
+    const now = new Date();
+    const currentHour = now.getHours();
+
+    // 현재 선택된 날짜의 시간대와 매칭되는 기온을 찾음
+    const timeIndex = selectedDayTimes.findIndex((time: string) => {
+      const date = new Date(time);
+      return date.getHours() === currentHour;
+    });
+
+    // 해당 시간대의 기온을 반환, 없다면 첫 번째 기온을 기본값으로 사용
+    return timeIndex !== -1
+      ? selectedDayTemperatures[timeIndex]
+      : selectedDayTemperatures[0];
+  };
   return (
     <WeatherContainer
       backgroundImage={getWeatherBackgroundImage(
@@ -247,54 +247,46 @@ const Home: React.FC = () => {
           </DaySummary>
         ))}
       </WeekWeatherSummary>
-      {geoData && geoData.length >= 3 ? (
-        <TodayMain>
-          {geoData[2]} {geoData[1]}&nbsp; 날씨:{todayMonth}.{todayDay} 기온:
-          {}°C &nbsp;
-          {getWeatherCode(weatherData.current_weather.weathercode)}
-          <br></br>
-          체감온도:
-          {temp(
-            weatherData.current_weather.temperature,
-            weatherData.current_weather.windSpeed
-          )}
-          °C &nbsp; 자외선 지수: {weatherUv(weatherData.daily.uv_index_max[0])}
-        </TodayMain>
-      ) : (
-        <h1>위치 데이터를 가져올 수 없습니다</h1>
-      )}
-      <TodayMain>
-        최고기온:{selectedDayWeather.maxTemp}°C &nbsp; 최저기온:
-        {selectedDayWeather.minTemp}°C
-        <br></br>
-        일출: 오전 {earlyTime(selectedDayWeather.sunrise[0])} &nbsp; 일몰: 오후
-        &nbsp;{earlyTime(weatherData.daily.sunset[0])}
-      </TodayMain>
       {showWeatherInfo && selectedDayWeather && (
         <div>
-          <h3>{selectedDayWeather.date.toLocaleDateString()} 날씨</h3>
-          <h4>최고 기온: {selectedDayWeather.maxTemp}°C</h4>
-          <h4>최저 기온: {selectedDayWeather.minTemp}°C</h4>
-          <h4>날씨: {getWeatherCode(selectedDayWeather.weatherCode)}</h4>
+          <TodayMain>
+            {geoData[2]} {geoData[1]}&nbsp; 날짜:{todayMonth}.{todayDay} 기온:
+            {getCurrentTemperature()}°C &nbsp;
+            <br></br>
+            최고 기온:
+            {selectedDayWeather.maxTemp}°C 최저 기온:
+            {selectedDayWeather.minTemp}°C 날씨:
+            {getWeatherCode(selectedDayWeather.weatherCode)}
+            <br></br>
+            일출: 오전 {earlyTime(selectedDayWeather.sunrise)} &nbsp; 일몰: 오후
+            &nbsp;{earlyTime(selectedDayWeather.sunset)}
+            &nbsp; 체감온도:
+            {temp(
+              weatherData.current_weather.temperature,
+              weatherData.current_weather.windSpeed
+            )}
+          </TodayMain>
           <WeatherLi>
             {selectedDayTimes.map((time: string, index: number) => (
               <li key={time}>
                 <h3>
                   {formatTimeTo12Hour(time)}: {selectedDayTemperatures[index]}°C
-                  &nbsp; 체감온도:{" "}
-                  {temp(selectedDayTemperatures[index], todayWindspeed[index])}
+                  &nbsp; 체감온도:
+                  {temp(
+                    selectedDayTemperatures[index],
+                    selectedDayWindspeed[index]
+                  )}
                   °C
                   <br></br>
-                  날씨: {getWeatherCode(todayWeatherCodes[index])}
+                  날씨: {getWeatherCode(selectedDayWeatherCodes[index])}
                   <br></br>
-                  습도:{todayHumidity[index]}%
+                  습도:{selectedDayHumidity[index]}%
                 </h3>
               </li>
             ))}
           </WeatherLi>
         </div>
       )}
-
       {showChart && <WeatherChart data={chartData} />}
     </WeatherContainer>
   );
