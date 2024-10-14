b25fcb2d5b108421695d913efc2d9ae86e2d6b4c
Sungho <hurgj123kr@gmail.com>
2024-10-14 19:48:25 +0900

    날씨별 옷 추천 조건문 개선

--- src/utils/weatherClothing.ts
+++ src/utils/weatherClothing.ts
@@ -1,13 +1,13 @@
 const weatherClothing = (temp: number) => {
-  if (temp >= 30 || temp > 25) {
+  if (temp >= 30 && temp > 25) {
     return "반팔 티셔츠, 반바지";
-  } else if (temp <= 24 || temp >= 20) {
+  } else if (temp < 25 && temp >= 20) {
     return "얇은 긴팔 티셔츠, 블라우스";
-  } else if (temp <= 19 || temp >= 15) {
+  } else if (temp < 20 && temp >= 15) {
     return "얇은 스웨터, 가벼운 자켓";
-  } else if (temp <= 14 || temp >= 10) {
+  } else if (temp < 15 && temp >= 10) {
     return "두꺼운 스웨터, 가벼운 코트";
-  } else if (temp <= 9 || temp >= 5) {
+  } else if (temp < 10 && temp >= 5) {
     return "코트,패딩,스카프,장갑";
   } else if (temp < 5 && temp >= 0) {
     return "두꺼운 패딩, 방한 모자, 장갑";
