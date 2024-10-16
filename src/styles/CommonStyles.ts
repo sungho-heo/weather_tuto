e64ee22618655f699f06d00edd91e6993b488834
Sungho <hurgj123kr@gmail.com>
2024-10-16 20:01:01 +0900

    readme update and 배경 흰화면 최대한 안보이게 해줌

--- src/styles/CommonStyles.ts
+++ src/styles/CommonStyles.ts
@@ -1,17 +1,14 @@
 import styled from "styled-components";
 
 export const WeatherContainer = styled.div<{ backgroundImage: string }>`
-  min-width: 100%;
-  height: 100%;
-  color: black;
-  font-weight: bold;
-  transition: background-color 0.3s ease-in-out;
-  background: linear-gradient(
-      rgba(255, 255, 255, 0.6),
-      rgba(255, 255, 255, 0.6)
-    ),
-    url(${(props) => props.backgroundImage});
+  background: url(${(props) => props.backgroundImage});
   background-size: cover; /* 이미지가 컨테이너를 꽉 채우도록 설정 */
   background-position: center; /* 이미지가 가운데 위치하도록 설정 */
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #d3d3d3; /* 배경 이미지가 없을 때 기본 회색 배경 추가 */
   background-blend-mode: overlay; /* 투명도 조정 */
+  font-weight: bold; /* 글씨 잘보이게 해줌.*/
 `;
