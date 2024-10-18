09ac62386305ab04b9f5809f9ff185d7573eed3d
Sungho <hurgj123kr@gmail.com>
2024-10-18 19:14:24 +0900

    날씨 이미지 업데이트

--- src/utils/weatherCode.ts
+++ src/utils/weatherCode.ts
@@ -35,33 +35,33 @@ export const getWeatherCode = (code: number) => {
 export const getWeatherBackgroundImage = (code: number): string => {
   switch (code) {
     case 0:
-      return "images/맑은날씨.jpeg"; // 맑은 날씨 이미지
+      return "images/맑음.jpeg"; // 맑은 날씨 이미지
     case 1:
     case 2:
     case 3:
-      return "images/흐린날씨.jpeg"; // 흐린 날씨 이미지
+      return "images/흐림.jpeg"; // 흐린 날씨 이미지
     case 45:
     case 48:
-      return "images/흐린날씨.jpeg"; // 안개 이미지
+      return "images/흐림.jpeg"; // 안개 이미지
     case 51:
     case 53:
     case 55:
-      return "images/비오는 날씨.gif"; // 이슬비 이미지
+      return "images/비.jpg"; // 이슬비 이미지
     case 61:
     case 63:
     case 65:
-      return "images/비오는 날씨.gif"; // 비 이미지
+      return "images/비.jpg"; // 비 이미지
     case 71:
     case 73:
     case 75:
-      return "images/눈오는 날씨.jpeg"; // 눈 이미지
+      return "images/눈.jpeg"; // 눈 이미지
     case 80:
     case 81:
     case 82:
-      return "images/비오는 날씨.gif"; // 소나기 이미지
+      return "images/비.jpg"; // 소나기 이미지
     case 95:
-      return "images/비오는 날씨.gif"; // 천둥번개 이미지
+      return "images/비.jpg"; // 천둥번개 이미지
     default:
-      return "images/맑은날씨.jpeg"; // 기본 이미지
+      return "images/맑음.jpeg"; // 기본 이미지
   }
 };
