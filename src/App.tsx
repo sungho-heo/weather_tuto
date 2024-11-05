57d055b9305548f810e370fe006dce5a4f5343b0
Sungho <hurgj123kr@gmail.com>
2024-11-05 20:11:05 +0900

    deploy update6

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { createGlobalStyle } from "styled-components";
+import { BrowserRouter } from "react-router-dom";
 import Home from "./components/Home";
 import { store } from "./store";
 
 const App: React.FC = () => {
   return (
-    <Provider store={store}>
-      <div>
-        <GlobalStyle />
-        <Home />
-      </div>
-    </Provider>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Provider store={store}>
+        <div>
+          <GlobalStyle />
+          <Home />
+        </div>
+      </Provider>
+    </BrowserRouter>
   );
 };
 
